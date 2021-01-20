4712b7dba3b106d50179e1c7d4b54f5148a3163b
thootau <acs106183@gm.ntcu.edu.tw>
2021-01-20 16:27:41 +0800

    ready for heroku deploy

--- serve.py
+++ serve.py
@@ -1,6 +1,5 @@
 import os
 from flask import Flask, render_template, request
-import requests
 from ganbun import input
 
 app = Flask(__name__)
@@ -13,5 +12,4 @@ def index():
 def chhoe():
     return input(request.args.get('bun'))
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8000, debug=True)
+if __name__ == '__main__': app.run(debug=True)
