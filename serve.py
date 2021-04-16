bc9d4c646be3235a89b33d6ac5225bec1a12c2bc
Thootau <thootau99@tutanota.com>
2021-04-16 15:21:51 +0800

    closing CORS

--- serve.py
+++ serve.py
@@ -1,11 +1,11 @@
 import os
 from flask import Flask, render_template, request, send_from_directory
-from flask_cors import CORS, cross_origin
+# from flask_cors import CORS, cross_origin
 
 from ganbun import input
 
 app = Flask(__name__, static_url_path='', static_folder='frontend/build')
-cors = CORS(app)
+# cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route("/", defaults={'path':''})
@@ -13,7 +13,7 @@ def serve(path):
     return send_from_directory(app.static_folder,'index.html')
 
 @app.route("/api/v1/chhoe")
-@cross_origin()
+# @cross_origin()
 def chhoe():
     result, _ = input(request.args.get('bun'))
     return result
